1f6a62b112bac146ad73cd2394eb266351b5d7e2
tomat-suppe <slak@itu.dk>
2026-03-25 13:27:02 +0100

    FIX: Removed console.log statements and the inline data (data will be provided by DB

--- replayerudf.js
+++ replayerudf.js
@@ -59,22 +59,6 @@ var valid = false;
 
 let gameInstance = new TicTacToe();
 
-let GAMEDATA =
-{
-"6":{"ID":1,"eventName":"Click","location":"restartButton","eventTime":5,"points":"n/a"},
-"7":{"ID":1,"eventName":"Click","location":3,"eventTime":6,"points":"n/a"},
-"8":{"ID":1,"eventName":"Click","location":4,"eventTime":7,"points":"n/a"},
-"9":{"ID":1,"eventName":"Click","location":1,"eventTime":8,"points":"n/a"},
-"10":{"ID":1,"eventName":"Click","location":7,"eventTime":9,"points":"n/a"},
-"11":{"ID":1,"eventName":"Click","location":5,"eventTime":10,"points":"n/a"},
-"12":{"ID":1,"eventName":"Click","location":0,"eventTime":11,"points":"n/a"},
-"13":{"ID":1,"eventName":"Click","location":8,"eventTime":12,"points":"n/a"},
-"14":{"ID":1,"eventName":"Click","location":6,"eventTime":13,"points":"n/a"},
-"15":{"ID":1,"eventName":"Click","location":2,"eventTime":14,"points":"n/a"},
-"16":{"ID":1,"gameEnd":true,"eventName":"Game over! X wins!","eventTime":14,"points":"n/a","highscore":"n/a"}
-} 
-
-
 for (const [_, eventObject] of Object.entries(GAMEDATA)) {
     switch (eventObject.eventName) {
         case "Click":
@@ -90,9 +74,7 @@ for (const [_, eventObject] of Object.entries(GAMEDATA)) {
                 if (eventObject.eventName === "Game is tied!") {
                     valid = gameInstance.checkTie();
                 } else {
-                    console.log("checking for win")
                     if (eventObject.eventName.includes("X wins!")) {
-                        console.log("checking for x win");
                         valid = gameInstance.checkWin('X');
                     } else if (eventObject.eventName.includes("O wins!")) {
                         valid = gameInstance.checkWin('O');
